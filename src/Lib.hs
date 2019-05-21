b933e6ac02f8e5c4327a3723ddba813f174b255f
Alex Tan <alex.r.tan@gmail.com>
2019-05-21 00:54:28 -0500

    Progress

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,7 +12,7 @@ import           TableDefinition                          ( tableSession )
 import qualified Generators.Api
 import qualified Generators.Types
 import qualified Generators.Decoders
-import           Elm                                      ( toString )
+import           Elm                                      ( toString, ModuleFile )
 import           Config
 import           System.Environment                       ( getEnv )
 import qualified Data.ByteString.Char8         as B
@@ -23,30 +23,40 @@ run = do
     connection <- Connection.acquire (B.pack dbURL)
     case connection of
         Right connection' -> do
-            let tableName = "words"
-            table' <- Session.run (tableSession tableName) connection'
 
-            case table' of
-                Right table'' -> do
-                    let config = TableConfig
-                            { specifiedTypeAlias       = Just "Word"
-                            , specifiedModuleNamespace = Nothing
-                            , table                    = table''
-                            }
-                    let a = toString $ Generators.Types.generate config
-                    let b = toString $ Generators.Decoders.generate config
-                    let c = toString $ Generators.Api.generate config
-
-                    case (a, b,c) of
-                        (Just a', Just b', Just c') -> do
-                            putStrLn c'
-                            -- writeFile "./output/Types.elm" a'
-                            -- writeFile "./output/Decoders.elm" b'
-                            -- writeFile "./output/Api.elm" c'
-                        _ -> do
-                            print a
-                            print b
-                            print c
-
-                Left _ -> return ()
+            let runner = runTable connection'
+            runner "words"
+            Connection.release connection'
         Left _ -> putStrLn "Couldn't establish connection to DB"
+
+generators :: [TableConfig -> Elm.ModuleFile]
+generators = 
+    [ Generators.Types.generate
+    , Generators.Decoders.generate
+    , Generators.Api.generate
+    ]
+
+runTable :: Connection.Connection -> String -> IO()
+runTable conn tableName = do
+    table' <- Session.run (tableSession tableName) conn
+
+    case table' of
+        Right table'' ->
+            let config = TableConfig
+                    { specifiedTypeAlias       = Just "Word"
+                    , specifiedModuleNamespace = Nothing
+                    , table                    = table''
+                    }
+            in
+            mapM_ (runGenerator config) generators
+
+        Left _ -> return ()
+
+runGenerator :: TableConfig -> (TableConfig -> Elm.ModuleFile) -> IO ()
+runGenerator config generator = do
+    let result = toString $ generator config
+    case result of
+        Just r -> do
+            putStrLn r
+        Nothing ->
+            return ()
